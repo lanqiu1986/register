9051e5a5fd819901b573defa556b693c6b108d3d
lanqiu1986 <lisuji1986@163.com>
2017-11-21 10:55:33 +0800

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ sh "ssh-add"
 lock "~> 3.10.0"
 
 set :application, "register"
-set :repo_url, "git@github.com:me/my_repo.git"
+set :repo_url, "git@github.com:lanqiu1986/register.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-
+set :deploy_to, "/home/deploy/register" #服务器上diamante位置
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -22,11 +22,12 @@ set :repo_url, "git@github.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+set :passenger_restart_with_touch, true
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
@@ -34,7 +35,7 @@ set :repo_url, "git@github.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
